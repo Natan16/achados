e87c197fdd57a9101a993ed0d091eb70285e4e48
natan <natanvianat16@gmail.com>
2020-11-11 20:22:12 -0300

    Atualiza requirements.txt com versoes que funcionam pra mim

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
 <div>  
   <v-layout justify-center align-center>
-    <v-btn color="rgba(255, 0, 0, 0.5)"  class="white--text" :to="{name: 'tipoRegistro-novo-registro', params:{tipoRegistro:'perdido'}}">Eu perdi um documento</v-btn>
+    <v-btn color="error"  class="white--text" :to="{name: 'tipoRegistro-novo-registro', params:{tipoRegistro:'perdido'}}">Eu perdi um documento</v-btn>
     <v-btn color="success" :to="{name: 'tipoRegistro-novo-registro', params:{tipoRegistro: 'achado'}}">Eu achei um documento</v-btn>
   	
   </v-layout>
@@ -10,7 +10,7 @@
 
     <div style="position: absolute; bottom: 5px; ">
      <h2>Nossa missão</h2> 
-    Dezenas de milhares de documentos são perdidos todo ano no Brasil. Muitas vezes, a pessoa que achou não se dá ao trabalho de levar o documento até o setor de achados e perdidos do orgão público resposável. E, ainda assim, a pessoa que perdeu tem que pagar alguma taxa. Essa plataforma tem como objetivo facilitar esse processo oferencendo um serviço GRATUITO para conectar quem perdeu a quem achou um documento.  
+    Dezenas de milhares de documentos são perdidos todo ano no Brasil. Muitas vezes, a pessoa que achou não se dá ao trabalho de levar o documento até o setor de achados e perdidos do orgão público resposável. E, ainda assim, a pessoa que perdeu tem que pagar alguma taxa. Essa plataforma tem como objetivo facilitar esse processo oferencendo, um serviço GRATUITO para conectar quem perdeu a quem achou um documento.  
     </div> 
    </div>
 
